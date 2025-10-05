82640fa133d0d7e2edbe34665eeba9be54b4c228
Cherry Lin <pdt8ye@virginia.edu>
2025-10-04 23:35:25 -0400

    Customize message for deployed Django app

--- ridesharing/views.py
+++ ridesharing/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, CS 3240 TA. This is Team A01 and this our Django base project deployed.")
