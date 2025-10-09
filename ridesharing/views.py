385fac4ba37dfc6adc2698ae672946d5b5e36aff
matthewjanicki <matthewjjanicki@gmail.com>
2025-10-09 11:53:48 -0400

    added user_profile to views.py

--- ridesharing/views.py
+++ ridesharing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,4 +6,5 @@ def index(request):
     return HttpResponse("Hello, CS 3240 TA. This is Team A01 and this our Django base project deployed.")
 
 def user_profile(request, username):
-    return 
+    user = get_object_or_404(User, username=username)
+    return render(request, "profile.html", {"user_profile": user})
