d81bfccf3d839a4ba9db0f13fe2b6db0662d21d0
matthewjanicki <matthewjjanicki@gmail.com>
2025-10-09 11:41:23 -0400

    added path for profile page

--- ridesharing/views.py
+++ ridesharing/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, CS 3240 TA. This is Team A01 and this our Django base project deployed.")
+
+def user_profile(request, username):
+    return 
