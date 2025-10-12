dd8e5ef63287b913c483e509153d4ef83db96443
Cherry Lin <pdt8ye@virginia.edu>
2025-10-11 20:20:56 -0400

    Adding landing page function and url pattern to dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home_dashboard, name="home")
+    path("home/", views.home_dashboard, name="home"),
+    path("landing/", views.landing, name="landing")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_dashboard(request):
     return render(request, "dashboard/index.html")
+
+def landing(request):
+    return render(request, "dashboard/landing.html")
