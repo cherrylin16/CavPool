310a6208e43b9d26d9713204f14e458d15aed3de
manavbharath <manavbh2011@gmail.com>
2025-10-28 00:02:05 -0400

    defaulting secret_key

--- project/settings.py
+++ project/settings.py
@@ -28,6 +28,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
     "DJANGO_SECRET_KEY",
+    default=secrets.token_urlsafe(nbytes=64),
 )
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
