8b6f7a2939cdf3e091181929f8aff2ff6e08eace
manavbharath <manavbh2011@gmail.com>
2025-10-30 11:23:28 -0400

    name on dashboard is name set on profile; defaults to username

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from accounts.models import RiderProfile, DriverProfile
 
-# Create your views here.
 def rider_dashboard(request):
-    return render(request, "dashboard/rider_dashboard.html")
+    profile = None
+    display_name = request.user.username
+    if request.user.is_authenticated:
+        try:
+            profile = RiderProfile.objects.get(user=request.user)
+            if profile.name:
+                display_name = profile.name
+        except RiderProfile.DoesNotExist:
+            pass
+    return render(request, "dashboard/rider_dashboard.html", {'display_name': display_name})
 
 def driver_dashboard(request):
-    return render(request, "dashboard/driver_dashboard.html")
+    profile = None
+    display_name = request.user.username
+    if request.user.is_authenticated:
+        try:
+            profile = DriverProfile.objects.get(user=request.user)
+            if profile.name:
+                display_name = profile.name
+        except DriverProfile.DoesNotExist:
+            pass
+    return render(request, "dashboard/driver_dashboard.html", {'display_name': display_name})
 
 def landing(request):
     return render(request, "dashboard/landing.html")
