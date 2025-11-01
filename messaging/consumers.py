e1ba308d386dbf06bbae1b889afe4e3bb08aadc3
matthewjanicki <matthewjjanicki@gmail.com>
2025-11-01 18:46:03 -0400

    updated message_detail.css to make messages actually visible

--- messaging/consumers.py
+++ messaging/consumers.py
@@ -3,33 +3,22 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        user = self.scope["user"]
-
-        other_user_id = self.scope["url_route"]["kwargs"]["user_id"]
-
-        user_ids = sorted([str(user.id), str(other_user_id)])
-        self.room_name = "_".join(user_ids)
+        self.other_user_id = self.scope["url_route"]["kwargs"]["user_id"]
+        self.room_name = f"chat_{min(self.scope['user'].id, int(self.other_user_id))}_{max(self.scope['user'].id, int(self.other_user_id))}"
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Join the chat group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message = data.get("message", "")
+        message = data["message"]
         sender = self.scope["user"].username
 
+        # broadcast to group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -40,7 +29,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def chat_message(self, event):
+        message = event["message"]
+        sender = event["sender"]
+
         await self.send(text_data=json.dumps({
-            "message": event["message"],
-            "sender": event["sender"],
+            "message": message,
+            "sender": sender,
         }))
