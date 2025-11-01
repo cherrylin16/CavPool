40dd98f67b6ecfe97eecc6e4eea230fad72abc4a
matthewjanicki <matthewjjanicki@gmail.com>
2025-11-01 14:27:42 -0400

    edited views.py

--- messages/views.py
+++ messages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 @login_required
@@ -8,7 +9,13 @@ def message_list(request):
     users = User.objects.exclude(id=request.user.id)
     return render(request, "messages/message_list.html", {"users": users})
 
+
 @login_required
-def message_detail(request, username):
-    other_user = get_object_or_404(User, username=username)
+def chat_room(request, user_id):
+    other_user = get_object_or_404(User, id=user_id)
     return render(request, "messages/message_detail.html", {"other_user": other_user})
+
+
+@login_required
+def new_message(request):
+    return redirect("message_page")
