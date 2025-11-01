62b24b90592636f2574078922ca1e2687d5eed54
matthewjanicki <matthewjjanicki@gmail.com>
2025-11-01 13:59:43 -0400

    created message_list.html based on rider_dashboard.html

--- messages/urls.py
+++ messages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("messages/",views.message_page, name="message_page"),
+    path("", views.messages_list, name="messages_list"),
+    path("<str:username>/", views.message_detail, name="message_detail"),
 ]
--- messages/views.py
+++ messages/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 @login_required
-def message_page(request):
-    return render(request, 'message_page.html')
+def messages_list(request):
+    users = User.objects.exclude(id=request.user.id)
+    return render(request, "messages/message_list.html", {"users": users})
+
+@login_required
+def message_detail(request, username):
+    other_user = get_object_or_404(User, username=username)
+    return render(request, "messages/message_detail.html", {"other_user": other_user})
