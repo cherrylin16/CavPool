7ff6fbbecce73cf00523761f95efc568423153b0
matthewjanicki <matthewjjanicki@gmail.com>
2025-11-01 15:02:42 -0400

    rollback point 1

--- messaging/views.py
+++ messaging/views.py
@@ -7,13 +7,13 @@ User = get_user_model()
 @login_required
 def message_list(request):
     users = User.objects.exclude(id=request.user.id)
-    return render(request, "messages/message_list.html", {"users": users})
+    return render(request, "messaging/message_list.html", {"users": users})
 
 
 @login_required
 def chat_room(request, user_id):
     other_user = get_object_or_404(User, id=user_id)
-    return render(request, "messages/message_detail.html", {"other_user": other_user})
+    return render(request, "messaging/message_detail.html", {"other_user": other_user})
 
 
 @login_required
