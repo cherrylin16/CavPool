d9613feec75cfc80ad20b86ddf95b28d6d614d17
manavbharath <manavbh2011@gmail.com>
2025-11-02 17:57:13 -0500

    fix migration dependency error

--- accounts/migrations/0003_user_is_moderator_alter_user_user_type.py
+++ accounts/migrations/0003_user_is_moderator_alter_user_user_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0002_driverprofile_riderprofile'),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
