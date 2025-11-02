98c4fc9cac8a3df86d62a8a85fc576190cc5b67a
charvikonduru <charvihansa.konduru@gmail.com>
2025-11-02 01:12:01 -0400

    added import for IntegrityERror

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, DriverProfile, RiderProfile
+from django.db import IntegrityError
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
