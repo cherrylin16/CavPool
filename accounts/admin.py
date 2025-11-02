6108d783fedfaa1917d5a9b8e2227ccae01d4fa7
charvikonduru <charvihansa.konduru@gmail.com>
2025-11-02 01:46:43 -0400

    i think the moderator is working now

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, DriverProfile, RiderProfile
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Extra fields', {'fields': ('user_type', 'is_moderator')}),
+    )
+    list_display = ('username', 'email', 'user_type', 'is_superuser', 'is_staff', 'is_active')
+    list_filter = ('user_type', 'is_superuser', 'is_staff', 'is_active')
