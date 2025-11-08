6adb352c8a3d49dc3711e585e0dfee432de59e06
manavbharath <manavbh2011@gmail.com>
2025-11-07 22:51:04 -0500

    created new ride_requests project

---
+++ ride_requests/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'ride_requests'
+
+urlpatterns = [
+    path('request/<int:post_id>/', views.request_ride, name='request_ride'),
+    path('manage/<int:post_id>/', views.manage_requests, name='manage_requests'),
+    path('update/<int:request_id>/', views.update_request_status, name='update_request_status'),
+]
