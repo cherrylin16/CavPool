d04f444ccf543cc942aac1ddfc30b6dc44ed3bb1
Cherry Lin <pdt8ye@virginia.edu>
2025-11-07 21:48:27 -0500

    Search bar functionality, refining post modals and forms, color themes

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,9 +4,15 @@ from .models import CarpoolPost
 class CarpoolPostForm(forms.ModelForm):
     class Meta:
         model = CarpoolPost
-        fields = ['text', 'image', 'image_visibility']
+        fields = ['name', 'date', 'pickup_time', 'dropoff_time', 'dropoff', 'pickup', 'notes', 'image', 'image_visibility']
         widgets = {
-            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Describe your carpool trip...'}),
+            'name': forms.Textarea(attrs={'class': 'form-control', 'rows':1, 'placeholder': 'Name of Carpool Trip'}),
+            'date': forms.Textarea(attrs={'class':'form-control', 'rows': 1, 'placeholder': 'Ex: Thursday, September 22'}),
+            'pickup_time': forms.Textarea(attrs={'class':'form-control', 'rows': 1, 'placeholder': 'Ex: 10:00 AM'}),
+            'dropoff_time': forms.Textarea(attrs={'class':'form-control', 'rows': 1, 'placeholder': 'Ex: 5:00 PM'}),
+            'dropoff': forms.Textarea(attrs={'class': 'form-control', 'rows': 1, 'placeholder': 'Street, City, State, Zip Code'}),
+            'pickup': forms.Textarea(attrs={'class': 'form-control', 'rows': 1, 'placeholder': 'Street, City, State, Zip Code'}),
+            'notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Describe additional details about your carpool trip...'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
             'image_visibility': forms.Select(attrs={'class': 'form-control'}),
         }
