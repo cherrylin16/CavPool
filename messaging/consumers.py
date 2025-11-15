02d7decd178d5baf80bf6985f8595bf87ce19ad4
matthewjanicki <matthewjjanicki@gmail.com>
2025-11-15 15:57:28 -0500

    added new notification consumer

--- messaging/consumers.py
+++ messaging/consumers.py
@@ -50,3 +50,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def get_user(self, user_id):
         User = get_user_model()
         return User.objects.get(id=user_id)
+    
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        user = self.scope["user"]
+        if not user.is_authenticated:
+            await self.close()
+            return
+
+        self.group_name = f"notify_{user.id}"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def notify(self, event):
+        await self.send(text_data=json.dumps({
+            "notification": True,
+            "from": event["from"]
+        }))
